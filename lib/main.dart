3ee48f80fd49a8f1010f74728506db12f49d70c4
rflypryg <rafly.535240004@stu.untar.ac.id>
2025-09-30 18:49:20 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,206 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart'; 
+import 'models/cart.dart'; 
+import 'screens/home_screen.dart';
+import 'screens/menu_screen.dart';
+import 'screens/cart_screen.dart';
+import 'screens/reservasi_screen.dart';
+import 'screens/about_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/signup_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => CartProvider(),
+      child: const LaperSpaceApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class LaperSpaceApp extends StatelessWidget {
+  const LaperSpaceApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'LaperSpace',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primarySwatch: Colors.orange,
+        scaffoldBackgroundColor: Colors.grey[50],
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black87,
+          elevation: 1,
+        ),
+        navigationRailTheme: NavigationRailThemeData(
+          selectedIconTheme: const IconThemeData(color: Colors.orange),
+          selectedLabelTextStyle: TextStyle(color: Colors.orange.shade700, fontWeight: FontWeight.bold),
+          unselectedIconTheme: const IconThemeData(color: Colors.grey),
+          unselectedLabelTextStyle: const TextStyle(color: Colors.black87),
+        )
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => const LoginScreen(),
+        '/signup': (context) => const SignupScreen(),
+        '/': (context) => const MainNavigationShell(), 
+      },
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class NavItemData {
+  final String label;
+  final IconData icon;
+  final Widget screen;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  const NavItemData({
+    required this.label,
+    required this.icon,
+    required this.screen,
+  });
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class MainNavigationShell extends StatefulWidget {
+  final int initialIndex;
+  const MainNavigationShell({super.key, this.initialIndex = 0});
 
-  void _incrementCounter() {
+  @override
+  State<MainNavigationShell> createState() => MainNavigationShellState();
+}
+
+class MainNavigationShellState extends State<MainNavigationShell> {
+  late int _selectedIndex;
+
+  final List<NavItemData> _navItemsData = const [
+    NavItemData(label: 'Home', icon: Icons.home, screen: HomeScreen()),
+    NavItemData(label: 'Menu', icon: Icons.restaurant_menu, screen: MenuScreen()),
+    NavItemData(label: 'Cart', icon: Icons.shopping_cart, screen: CartScreen()), 
+    NavItemData(label: 'Reservasi', icon: Icons.table_bar, screen: ReservasiScreen()),
+    NavItemData(label: 'Contact', icon: Icons.phone, screen: ContactUsScreen()), 
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedIndex = widget.initialIndex;
+  }
+
+  void _onItemTapped(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _selectedIndex = index;
     });
   }
+  
+  Widget _buildCartIcon(BuildContext context, bool isSelected) {
+    return Consumer<CartProvider>(
+      builder: (context, cart, child) {
+        final itemCount = cart.itemCount;
+        final color = isSelected ? Colors.orange : Colors.grey;
+
+        return Stack(
+          alignment: Alignment.center,
+          children: [
+            Icon(_navItemsData[2].icon, color: color), // Icon Cart
+            if (itemCount > 0)
+              Positioned(
+                top: 0,
+                right: 0,
+                child: Container(
+                  padding: const EdgeInsets.all(3),
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  constraints: const BoxConstraints(
+                    minWidth: 18,
+                    minHeight: 18,
+                  ),
+                  child: Text(
+                    itemCount.toString(),
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 10,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              )
+          ],
+        );
+      },
+    );
+  }
+
+  BottomNavigationBarItem _buildBottomNavItem(NavItemData item, int index) {
+    if (item.label == 'Cart') {
+      return BottomNavigationBarItem(
+        icon: _buildCartIcon(context, _selectedIndex == index),
+        label: item.label,
+      );
+    }
+    return BottomNavigationBarItem(icon: Icon(item.icon), label: item.label);
+  }
+
+  NavigationRailDestination _buildRailDestination(NavItemData item, int index) {
+    if (item.label == 'Cart') {
+      return NavigationRailDestination(
+        icon: _buildCartIcon(context, _selectedIndex == index),
+        selectedIcon: _buildCartIcon(context, true), 
+        label: Text(item.label),
+      );
+    }
+    return NavigationRailDestination(
+      icon: Icon(item.icon),
+      selectedIcon: Icon(item.icon, color: Colors.orange),
+      label: Text(item.label),
+    );
+  }
+
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final isMobile = MediaQuery.of(context).size.width < 700;
+
+    final List<Widget> screens = _navItemsData.map((item) => item.screen).toList();
+    final List<BottomNavigationBarItem> bottomNavItems = 
+        _navItemsData.asMap().entries.map((entry) => _buildBottomNavItem(entry.value, entry.key)).toList();
+    final List<NavigationRailDestination> railDestinations = 
+        _navItemsData.asMap().entries.map((entry) => _buildRailDestination(entry.value, entry.key)).toList();
+
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      body: Row(
+        children: [
+          if (!isMobile)
+            NavigationRail(
+              selectedIndex: _selectedIndex,
+              onDestinationSelected: _onItemTapped,
+              labelType: NavigationRailLabelType.selected,
+              destinations: railDestinations,
             ),
-          ],
-        ),
+  
+          Expanded(
+            child: IndexedStack(
+              index: _selectedIndex,
+              children: screens,
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+      bottomNavigationBar: isMobile
+          ? BottomNavigationBar(
+              items: bottomNavItems,
+              currentIndex: _selectedIndex,
+              selectedItemColor: Colors.orange,
+              unselectedItemColor: Colors.grey,
+              onTap: _onItemTapped,
+              type: BottomNavigationBarType.fixed,
+              selectedFontSize: 12,
+            )
+          : null,
     );
   }
 }
